049a9c2acfd722c9fc6f4ce22769413cc7fb8acb
Юра Хитрень <dvigokgp@gmail.com>
2022-12-18 16:31:47 +0200

    start

---
+++ ArtSoftUi/ArtSoftUi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ArtSoftUi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
